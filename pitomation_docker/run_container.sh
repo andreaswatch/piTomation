ea4ec4c1dc31177459d789d6469112035624b578
andreaswatch <strauss.mobil@googlemail.com>
2022-03-02 20:03:47 +0000

    update run_container.sh

--- pitomation_docker/run_container.sh
+++ pitomation_docker/run_container.sh
@@ -1,7 +1,15 @@
 # --name piTomation \
+
+#docker run -it \
+# -v "$PWD":/home/pi/piTomation \
+# --mount type=bind,source="$(pwd)"/../,target=/app \
+# --mount type=bind,source="$(pwd)"/home/pi/piTomation/src/frontdoor.yaml,target=/app/config.yaml \
+# -w /home/pi/piTomation awatch/pitomation:1.0.2 python\
+# /app/__main__.py /app/config.yaml
+
 docker run -it \
- -v "$PWD":/home/pi/piTomation \
- --mount type=bind,source="$(pwd)"/../,target=/app \
- --mount type=bind,source="$(pwd)"/home/pi/frontdoor.yaml,target=/app/config.yaml \
- -w /home/pi/piTomation awatch/pitomation:1.0.2 python\
- /app/__main__.py /app/config.yaml
+  --mount type=bind,source=/home/pi/piTomation,target=/app \
+  --mount type=bind,source=/home/pi/frontdoor.yaml,target=/config.yaml \
+  --privileged \
+  awatch/pitomation:1.0.2 python \
+  /app/__main__.py /config.yaml
